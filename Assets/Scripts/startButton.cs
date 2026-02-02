2534d88c56d79aa568c8db999b50b64c788db731
DESKTOP-57M2H7S\game <chop7410@naver.com>
2026-02-02 18:05:04 +0900

    글자에 애니메이션 넣기 및 씬 이동 구현

--- Assets/Scripts/startButton.cs
+++ Assets/Scripts/startButton.cs
@@ -1,16 +1,12 @@
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class startButton : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    
+    public void OnStartButtonClick()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene("MainScene");
+        Debug.Log("메인씬 실행");
     }
 }
