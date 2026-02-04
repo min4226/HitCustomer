4465ee0366942d38091a9069c179bdd2923d479d
DESKTOP-57M2H7S\game <chop7410@naver.com>
2026-02-04 16:45:34 +0900

    내 마트로 바로가기 씬 구성

---
+++ Assets/Scripts/OnTemaClick.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class OnTemaClick : MonoBehaviour
+{
+   public GameObject otherGameObject;         
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
---
+++ Assets/Scripts/ToMyMarket.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class ToMyMarket : MonoBehaviour
+{
+    public void OnMyMarket()
+    {
+        SceneManager.LoadScene("MyMarket");
+    }
+}
